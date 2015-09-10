dcd63abd143e3ff5f1c325bb6d7cc0a5f234b18f
A <s>
2015-09-10 11:21:27 +0300

    Correction for elisp mode and extend gitignore.

--- elisp/elisp-config.el
+++ elisp/elisp-config.el
@@ -2,7 +2,7 @@
 ;;emacs lisp
 (defun elisp-mode-complex-hook()
   (yas-minor-mode)
-  (define-key elisp-mode-map [tab] 'yas/expand)
+  (define-key js-mode-map [tab] 'yas/expand)
   (setq yas/after-exit-snippet-hook 'indent-according-to-mode)
 )
 (add-hook 'emacs-lisp-mode-hook 'elisp-mode-complex-hook)
