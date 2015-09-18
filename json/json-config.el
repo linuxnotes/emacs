d76c0b877f0b63ff3679f8c37dd17712f47690a7
A <s>
2015-09-18 11:48:45 +0300

    Correction for json mode.

--- json/json-config.el
+++ json/json-config.el
@@ -2,4 +2,7 @@
 
 (modify-coding-system-alist 'file "\\.json\\'" 'utf-8) ;; json файлы в utf-8
 (require 'json-mode)
+
+;; set for correct encoding without \u
+(setq json-reformat:pretty-string? 't)
 (provide 'json-config)
