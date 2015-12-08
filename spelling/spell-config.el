ee64c5dd2df79bbf4f06daa154a956085047cd63
A <s>
2015-12-08 15:03:11 +0300

    Add description for using the ispell mode with aspell program.

--- spelling/spell-config.el
+++ spelling/spell-config.el
@@ -14,6 +14,41 @@
 (setq-default ispell-program-name "ispell")
 (setq ispell-local-dictionary "russian")
 
+;; for aspell 
+;; http://filonenko-mikhail.blogspot.ru/2012/03/emacs.html
+;; need install aspell aspell-ru aspell-en
+;; su
+;; cd /usr/lib/aspell
+;; cp ru.dat ru_backup.dat
+;; grep '^special' en.dat >>ru.dat
+;; aspell dump master en >w.en
+;; aspell dump master ru-yo >w.ru
+;; cat w.ru w.en >w.all
+;; aspell --lang=ru --encoding=UTF-8 create master ruen.rws < w.all
+;; rm -f w.ru w.en w.all
+;; echo "add ruen.rws" > ru.multi
+;; exit
+
+;; ;; configuration
+;; (setq ispell-highlight-face (quote flyspell-incorrect))
+;; (setq ispell-have-new-look t)
+;; (setq ispell-enable-tex-parser t)
+;; (add-hook 'text-mode-hook 'flyspell-mode)
+;; (add-hook 'latex-mode-hook 'flyspell-mode)
+;; (setq flyspell-delay 1)
+;; (setq flyspell-always-use-popup t)
+
+;; (setq flyspell-issue-welcome-flag nil)
+;; Need change dictionry to russian for working
+
+;; M+x ispell-change-dictionary RET
+;; ru RET
+;; M+x ispell
+
+;; (setq
+;;  ispell-program-name "aspell"
+;;  ispell-extra-args '("--sug-mode=ultra"))
+
 ;; add for text mode
 (dolist (hook '(text-mode-hook))
       (add-hook hook (lambda () (flyspell-mode 1))))
