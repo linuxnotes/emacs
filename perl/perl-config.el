27799c9a0b92c8d623d0ed61c3faf8dd4791d312
linuxnotes <linux.notes@none>
2017-03-15 22:43:24 +0300

    Code correction for perl mode config.

--- perl/perl-config.el
+++ perl/perl-config.el
@@ -3,11 +3,8 @@
 ;; git pull https://github.com/Perl-Critic/Perl-Critic.git ~/git/
 ;; chmod +x ~/git/Perl-Critic/bin/perlcritic
 
-
-;;; cperl-mode is preferred to perl-mode
-;;(defalias 'perl-mode 'cperl-mode)
-
 (add-to-list 'load-path "~/.emacs.d/imenu")
+
 (require 'imenu-tree)
 (load-file "~/.emacs.d/imenu/imenu-tree.el") 
 
@@ -24,6 +21,7 @@
 			 nil)
 		   (flymake-mode)
 		   (yas-minor-mode)
+		   (hs-minor-mode)
 		   
 		   (define-key perl-mode-map (kbd "C-x i") 'yas-expand) ;; redifine insert-file that not used
 		   (define-key perl-mode-map (kbd "C-c x") 'imenu-tree) ;; redifine insert-file that not used
@@ -35,33 +33,38 @@
 		)
 	  (add-hook 'perl-mode-hook 'perl-mode-complex-hook)
 	  (add-hook 'cperl-mode-hook 'perl-mode-complex-hook)
+
 	  (add-to-list 'load-path "~/git/Perl-Critic/extras/")
 	  (require 'perlcritic)
+
 	  ;; If flymake_perlcritic isn't found correctly, specify the full path
-	  (setq flymake-perlcritic-command
-			"~/.emacs.d/perl/bin/flymake_perlcritic")
+	  (setq flymake-perlcritic-command "~/.emacs.d/perl/bin/flymake_perlcritic")
+
 	  ;; Lets set it to be the most severe available.
 	  (setq flymake-perlcritic-severity 4)
+
 	  ;; If you don't want to use the default ~/.perlcriticrc
 	  ;;(setq flymake-perlcritic-profile "~/projects/big-project/perlcriticrc")
+
 	  (require 'flymake-perlcritic)
-	  ;;
+	  
+	  ;;set some paths
 	  (setenv "PATH"
 			  (concat
 			   (expand-file-name "~/perl5/bin") ":"
 			   (expand-file-name "~/git/Perl-Critic/bin/") ":"
-			   (getenv "PATH")
-			   ))
+			   (getenv "PATH")))
+
 	  (setenv "PERL5LIB"
 			  (concat
 			   (expand-file-name "~/perl5/lib/perl5") ":"
 			   (expand-file-name "~/perl5") ":"
 			   (getenv "PERL5LIB")))
+
 	  (setenv "PERL_LOCAL_LIB_ROOT"
 			  (concat
 			   (expand-file-name "~/perl5") ":")
-			  (getenv "PERL_LOCAL_LIB_ROOT"))
-	  )
+			  (getenv "PERL_LOCAL_LIB_ROOT")))
   nil)
 
 (setq
