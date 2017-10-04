b04ea9cbfd66f3f21854dcaefbdc292f9fe439dd
linuxnotes <linux.notes@none>
2017-10-04 13:47:24 +0300

    Json libs placed to submodules.

--- json/json-config.el
+++
@@ -1,8 +0,0 @@
-;;;; Настройки для работы с json
-
-(modify-coding-system-alist 'file "\\.json\\'" 'utf-8) ;; json файлы в utf-8
-(require 'json-mode)
-
-;; set for correct encoding without \u
-(setq json-reformat:pretty-string? 't)
-(provide 'json-config)
