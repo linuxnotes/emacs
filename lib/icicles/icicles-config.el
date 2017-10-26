2a5481bb1fc8a175a4debbca9606b372c3609d73
linuxnotes <linux.notes@none>
2017-10-26 18:12:06 +0300

    Icicles.

---
+++ lib/icicles/icicles-config.el
@@ -0,0 +1,7 @@
+;; other helping module is guide-key.el
+;; that required popwin.el
+
+(add-to-list 'load-path "~/.emacs.d/lib/icicles/icicles")
+(require 'icicles)
+(icy-mode 1)
+(provide 'icicles-config)
